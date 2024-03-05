52856d61caa01f5ba87ceebdfb58cadc59d50301
Yxssssmin <yasmintate02@gmail.com>
2024-03-06 00:25:07 +0100

    Añadir producto

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -11,4 +11,24 @@ class CrudController extends Controller
         $datos=DB::select(" select * from producto ");
         return view("welcome")->with("datos", $datos);
     }
+
+    public function create(Request $request) {
+        try {
+            $sql=DB::insert(" insert into producto(id_producto,nombre,precio,cantidad)values(?,?,?,?) ", [
+                $request->txtcodigo,
+                $request->txtnombre,
+                $request->txtprecio,
+                $request->txtcantidad,
+            ]);
+        } catch (\Throwable $th) {
+            $sql = 0;
+        }
+
+        if($sql == true) {
+            return back()->with("correcto","Producto registrado correctamente");
+        } else {
+            return back()->with("incorrecto","Error al registrar");
+        }
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/", [CrudController::class, "index"])->name("crud.index");
+
+// ruta para añadir un nuevo producto
+Route::post("/registrar-producto", [CrudController::class, "create"])->name("crud.create");
